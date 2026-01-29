f2f6f404f1b4c0e147616b055a7a696242cb075b
Aung Thu Hein <agthuhein.mm@gmail.com>
2026-01-29 15:07:40 +0100

    Completed DTO's, controllers, service classes, and repositories

--- app/src/main/java/com/project/back_end/repo/PrescriptionRepository.java
+++ app/src/main/java/com/project/back_end/repo/PrescriptionRepository.java
@@ -2,11 +2,12 @@ package com.project.back_end.repo;
 
 import com.project.back_end.models.Prescription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface PrescriptionRepository extends JpaRepository<Prescription, String> {
+public interface PrescriptionRepository extends MongoRepository<Prescription, String> {
     List<Prescription> findByAppointmentId(Long appointmentId);
 }
 
