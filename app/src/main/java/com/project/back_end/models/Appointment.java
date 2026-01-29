12d9dd4bb8d48a15403296c5196b0506904d24f6
Aung Thu Hein <agthuhein.mm@gmail.com>
2026-01-29 09:17:38 +0100

    edit models

--- app/src/main/java/com/project/back_end/models/Appointment.java
+++ app/src/main/java/com/project/back_end/models/Appointment.java
@@ -44,5 +44,45 @@ public class Appointment {
     public LocalTime getAppointmentTimeOnly() {
         return appointmentTime.toLocalTime();
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Doctor getDoctor() {
+        return doctor;
+    }
+
+    public void setDoctor(Doctor doctor) {
+        this.doctor = doctor;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public LocalDateTime getAppointmentTime() {
+        return appointmentTime;
+    }
+
+    public void setAppointmentTime(LocalDateTime appointmentTime) {
+        this.appointmentTime = appointmentTime;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
 }
 
